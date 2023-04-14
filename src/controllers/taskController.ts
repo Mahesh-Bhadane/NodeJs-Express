913a219d623c10f6b5d6e7e221c137669124e68c
Mahesh-Bhadane <maheshbhadane708@gmail.com>
2023-04-14 10:29:56 +0530

    minor file changes

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -4,7 +4,7 @@ import { City, Task } from "../types/types";
 import multer from "multer";
 import path from 'path';
 import fs from 'fs';
-import { SendEmail } from "../sendMail";
+import { SendEmail } from "../utils/sendMail";
 
 // get all tasks
 export const taskName = (req:Request,res:Response) => {
