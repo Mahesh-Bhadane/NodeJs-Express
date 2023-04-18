5c63e3f881af6b53985a09abf1eb19429ec62465
Mahesh-Bhadane <maheshbhadane708@gmail.com>
2023-04-18 12:13:51 +0530

    implemented lambda function

--- server.ts
+++ server.ts
@@ -5,19 +5,30 @@ import cityRouter from './src/routers/cityRouter';
 import taskRouter from './src/routers/taskRouter';
 import { corsOptions } from './src/config/corsOption';
 import cors from "cors";
+import awsServerlessExpress from 'aws-serverless-express';
 
-
-//create app instance
+// Create an instance of the Express app
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+// Add CORS middleware
 app.use(cors(corsOptions));
 
+// Add JSON body parser middleware
 app.use(express.json());
 
-app.use('/api/auth',authRouter );
-app.use('/api/city',cityRouter );
-app.use('/api/task',taskRouter );
+// Add your routers
+app.use('/api/auth', authRouter);
+app.use('/api/city', cityRouter);
+app.use('/api/task', taskRouter);
+
+// Create a Lambda handler function
+const server = awsServerlessExpress.createServer(app);
+
+// Define the Lambda function handler
+export const handler = (event: any, context: any) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
